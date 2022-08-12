c9015a286663770d3fc90dc1e364bc7543d3b955
Rufaidat <alsiddiqrufaidat@gmail.com>
2022-08-12 12:14:16 +0100

    part1

---
+++ part0/play.js
@@ -0,0 +1,9 @@
+const arto = {
+  name: "Arto Hellas",
+  age: 35,
+  education: "PhD",
+  greet: function () {
+    console.log("hello, my name is " + this.name);
+  },
+};
+arto.greet();
--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+const Display = (props) => {
+  return <div>{props.counter}</div>;
+};
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const plus1 = () => setCounter(counter + 1);
+  const minus1 = () => setCounter(counter - 1);
+  const reset = () => setCounter(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Display counter={counter} />
+      <Button onClick={plus1} text="plus" />
+      <Button onClick={minus1} text="minus" />
+      <Button onClick={reset} text="reset" />
     </div>
   );
-}
+};
 
 export default App;
