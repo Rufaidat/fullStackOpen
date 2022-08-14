4d2214c2893fc95775997eea1fd4d95c1906c9c6
Rufaidat <alsiddiqrufaidat@gmail.com>
2022-08-15 00:06:26 +0100

    anecdotes

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const Heading = (props) => <h1>{props.text}</h1>;
 
-function App() {
+const StatisticLine = (props) => (
+  <table>
+    <tbody>
+      <tr>
+        <td>{props.text} </td>
+        <td>{props.value}</td>
+      </tr>
+    </tbody>
+  </table>
+);
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+
+  const handleGoodClick = () => {
+    setAll(all + 1);
+    setGood(good + 1);
+  };
+  const handleNeutralClick = () => {
+    setAll(all + 1);
+    setNeutral(neutral + 1);
+  };
+  const handleBadClick = () => {
+    setAll(all + 1);
+    setBad(bad + 1);
+  };
+
+  const Statistics = (props) => {
+    if (props.total === 0) {
+      return <div>No Feedback given</div>;
+    }
+    return (
+      <div>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine
+          text="average"
+          value={(good * 1 + neutral * 0 + bad * -1) / 3}
+        />
+        <StatisticLine text="positive" value={`${(good / all) * 100}%`} />
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading text="give feedback" />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+      <Heading text="statistics" />
+      <Statistics total={all} />
     </div>
   );
-}
-
+};
 export default App;
