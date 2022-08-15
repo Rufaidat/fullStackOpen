d4851632ea9bf495faf6885184f42427217992e0
Rufaidat <alsiddiqrufaidat@gmail.com>
2022-08-15 01:12:19 +0100

    info

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,17 +1,65 @@
-import { useState } from "react";
-const Display = ({ counter }) => <div>{counter}</div>;
-const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
-const App = () => {
-  const [counter, setCounter] = useState(0);
-  const plus1 = () => setCounter(counter + 1);
-  const minus1 = () => setCounter(counter - 1);
-  const reset = () => setCounter(0);
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Content = (props) => {
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={plus1} text="plus" />
-      <Button onClick={minus1} text="minus" />
-      <Button onClick={reset} text="reset" />
+      <Part
+        part={props.part["parts"][0]["name"]}
+        number={props.part["parts"][0]["exercises"]}
+      />
+      <Part
+        part={props.part["parts"][1]["name"]}
+        number={props.part["parts"][1]["exercises"]}
+      />
+      <Part
+        part={props.part["parts"][2]["name"]}
+        number={props.part["parts"][2]["exercises"]}
+      />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <div>
+      {props.part} {props.number}
+    </div>
+  );
+};
+
+const Total = (props) => (
+  <div>
+    Number of exercises{" "}
+    {props.total["parts"][0]["exercises"] +
+      props.total["parts"][1]["exercises"] +
+      props.total["parts"][2]["exercises"]}
+  </div>
+);
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course["name"]} />
+      <Content part={course} />
+      <Total total={course} />
     </div>
   );
 };
